14a3d4ea947d76ccfe17c6afe98780f236ba1eed
Hack Reactor Student <student@hackreactor.com>
2017-04-12 20:21:04 -0700

    Set state and click event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,44 @@
-var App = (props) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={props.video}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={props.exampleVideoData}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: window.exampleVideoData,
+      currentVideo: window.exampleVideoData[0]
+    }
+  }
+
+  onClick(event) {
+    // grab title
+    // search exampleVideoData for title
+    var index;
+    for (var i = 0; i < window.exampleVideoData.length; i++) {
+      if (window.exampleVideoData[i].etag === event.target.id) {
+        index = i;
+      }
+    }
+    // set current song to clicked song index
+    this.setState({
+      currentVideo: window.exampleVideoData[index]  
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7"> 
+          <VideoPlayer video={this.state.currentVideo} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videos} onClick={this.onClick.bind(this)} />
+        </div>
+      </div>
+    );
+  }
+
+};
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
