50f20ecb340a4b418bdb984e659b851a2cabb15c
Hack Reactor Student <student@hackreactor.com>
2017-04-13 16:24:38 -0700

    Fix syntax error for searchYouTube

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-ReactDOM.render(<App exampleVideoData={exampleVideoData} />, document.getElementById('app'));
+ReactDOM.render(<App searchYouTube={window.searchYouTube} />, document.getElementById('app'));
