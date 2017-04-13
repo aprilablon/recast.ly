2d96a456c76dd1680f90e769446d341fd3aa172f
Hack Reactor Student <student@hackreactor.com>
2017-04-13 12:20:00 -0700

    Add youtube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,18 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({key, query, max=5}, callback) => {
+  $.ajax({
+    type: 'GET',
+    part: 'snippet',
+    videoEmbeddable: true,
+    contentType: 'application/json; charset=utf-8',
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + query + '&maxResults=' + max + '&key=' + key + '&type=video&videoEmbeddable=true',
+    success: function(response) {
+      console.log('Working fine! Damn fine. :)');
+      callback(response.items);
+    },
+    error: function() {
+      console.log('Bad resquest :(');
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
